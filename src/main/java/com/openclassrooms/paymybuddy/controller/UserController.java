ca1aab77b55271095394920e31bd63173b707ebc
Barbara Calas <bcalas@finwedge.com>
2023-06-20 11:00:32 +0200

    css home page and navbar

--- src/main/java/com/openclassrooms/paymybuddy/controller/UserController.java
+++ src/main/java/com/openclassrooms/paymybuddy/controller/UserController.java
@@ -1,16 +1,25 @@
 package com.openclassrooms.paymybuddy.controller;
 
+import com.openclassrooms.paymybuddy.model.User;
+import com.openclassrooms.paymybuddy.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user_page")
+@RequestMapping("/home")
 public class UserController {
+    @Autowired
+    UserServiceImpl userService;
 
     @GetMapping()
-    public String registrationForm() {
-        return "user";
+    public String registrationForm(Model model) {
+
+        User currentUser = userService.getCurrentUser();
+        model.addAttribute("currentUser", currentUser);
+        return "home";
     }
 
 
